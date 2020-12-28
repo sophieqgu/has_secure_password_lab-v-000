54da1befa3f80a6be12936c72dad681a31cfeeb7
Sophie Gu <sophieqgu@gmail.com>
2020-12-28 04:17:10 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    if @user
+    if @user && user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to root_path
     else
       redirect_to new_user_path
-    end 
+    end
   end
 
   def invalid_params
