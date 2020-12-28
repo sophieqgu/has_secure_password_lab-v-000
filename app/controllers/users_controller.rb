3f24b595ebba330aaf70ff2ed1fbc4ad3cb5df8e
Sophie Gu <sophieqgu@gmail.com>
2020-12-28 03:55:23 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    if !params.valid
+      redirect_to login_path
+    else
+      @user = User.create(user_params)
+      redirect_to root_path
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end 
 end
