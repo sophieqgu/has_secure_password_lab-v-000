b43d16b1922e8ef00831e470c0995a746b1303db
Sophie Gu <sophieqgu@gmail.com>
2020-12-28 03:59:03 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,19 @@
 class SessionsController < ApplicationController
 
   def new
-    end
-
-    def create
-      @user = User.find_by(name: params[:name])
-      redirect_to login_path unless @user.authenticate(params[:password])
-      session[:name] = params[:name]
-      redirect_to root_path
-    end
-
-    def destroy
-      session.delete :name
-      redirect_to root_path
-    end
   end
+
+  def create
+    @user = User.find_by(name: params[:name])
+    redirect_to login_path unless @user.authenticate(params[:password])
+    session[:name] = params[:name]
+    redirect_to root_path
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to root_path
+  end
+
 
 end
